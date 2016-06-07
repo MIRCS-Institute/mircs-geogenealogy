467143120486d86be522535a6b6213f57daf4c49
Brad Covey <bradcovey@gmail.com>
2016-06-07 13:25:39 -0300

    Added an MVC for uploading a CSV
    TODO: change upload_csv POST response to write file to MEDIA_ROOT
          and set stored filename in request.session
    TODO: add MEDIA_ROOT
    TODO: add set_primary_key view to open the CSV from MEDIA ROOT,
          create a database table, and store the contents of the file
    TODO: change upload_csv POST response to return some JSON with the
          CSV column headers and indices as well as the first few rows
          of data.
    TODO: add to upload_csv template to display the upload_csv response

--- mircsgeo/website/urls.py
+++ mircsgeo/website/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^home$', views.home, name='home')
+    url(r'^home$', views.home, name='home'),
+    url(r'^upload_csv$', views.upload_csv, name='upload_csv'),
+    url(r'^test_response$', views.test_response, name='test_response')
 ]
--- mircsgeo/website/views.py
+++ mircsgeo/website/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.template import RequestContext
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 
 import website.models
+from .forms import UploadCsv
 
 import pandas as pd
 
@@ -9,6 +11,22 @@ import pandas as pd
 def home(request):
     context = {}
     return render(request, 'home.html', context)
+
+
+def upload_csv(request):
+    if request.method == 'POST':
+        # Do some stuff
+        form = UploadCsv(request.POST, request.FILES)
+        if form.is_valid():
+            request.session['data'] = pd.read_csv(request.FILES['csv_file'])
+            return HttpResponseRedirect('/test_response')
+    else:
+        form = UploadCsv()
+    return render(request, 'upload_csv.html', {'form': form})
+
+
+def test_response(request):
+    return HttpResponse('yay')
 
 
 def Session():
