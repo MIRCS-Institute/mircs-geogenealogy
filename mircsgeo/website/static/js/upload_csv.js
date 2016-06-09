3c0f5d0221c7fc39cafee36e3975870c3c992c59
Brad Covey <bradcovey@gmail.com>
2016-06-09 12:58:06 -0300

    Added a display for CSV file contents

--- mircsgeo/website/static/js/upload_csv.js
+++ mircsgeo/website/static/js/upload_csv.js
@@ -1,7 +1,6 @@
 $( document ).ready(function() {
   $('#csvUploadForm #submitButton').click( function() {
     var formData = new FormData($('#csvUploadForm')[0]);
-    // var formData =  $('#csvUploadForm').serialize();
     console.log(formData);
     $.ajax({
       url: 'store_csv',
@@ -12,9 +11,33 @@ $( document ).ready(function() {
       contentType: false,
       processData: false,
       success: function(data) {
-        console.log(data);
+        populateDataTable($('#uploadedDataTable'), data['columns'], data['rows'])
       }
     });
     return false;
   });
 })
+
+function populateDataTable(tableElement, columns, rows) {
+  // Iterate columns and create table headers for each
+  var headerRow = $('<tr></tr>');
+  for(var i=0; i<columns.length; i++) {
+    headerRow.append('<th>' + columns[i] + '</th>');
+  }
+  tableElement.append(headerRow);
+
+  // Iterate rows of data and create table rows for each
+  for(var i=0; i<rows.length; i++) {
+    // step through rows of data, convert each to an html table row, append to the table
+    tableElement.append(createTableRow(rows[i]));
+  }
+}
+
+function createTableRow(dataRow) {
+  var tableRow = $('<tr></tr>');
+  for(var i=0; i<dataRow.length; i++) {
+    // Create a table element for every cell
+    tableRow.append('<td>' + dataRow[i] + '</td>');
+  }
+  return tableRow;
+}
